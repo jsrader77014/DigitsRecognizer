a6b21e45ccdb9f309d0c132ec5eb1ddd73b8542f
Jessie Srader <gareth_trilogy@hotmail.com>
2018-10-27 17:17:10 -0500

    Added F# Library to take advantage of interactive features

---
+++ FSharp/script.fsx
@@ -0,0 +1,14 @@
+﻿open System.IO
+type Observation = { Label:string; Pixels: int[] }
+
+let toObservation (csvData:string) =
+    let columns = csvData.Split(',')
+    let label = columns.[0]
+    let pixels = columns.[1..] |> Array.map int
+    { Label = label; Pixels = pixels }
+let reader path =
+    let data = File.ReadAllLines path
+    data.[1..]
+    |> Array.map toObservation
+let trainingPath = @"C:\Users\garet\Source\Repos\DigitsRecognizer\DigitsRecognizer\bin\Debug\netcoreapp2.1\train.csv"
+let trainingData = reader trainingPath
